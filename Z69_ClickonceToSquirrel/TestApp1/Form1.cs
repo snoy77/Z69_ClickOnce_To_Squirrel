18945c7bdc310163bde7ba0d5ab838401249c0fb
Snoy77 <Snoy77@mail.ru>
2021-09-28 10:02:20 +0500

    Обновил и опубликовал приложение

--- Z69_ClickonceToSquirrel/TestApp1/Form1.cs
+++ Z69_ClickonceToSquirrel/TestApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,17 @@ namespace TestApp1
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world!");
         }
     }
 }
